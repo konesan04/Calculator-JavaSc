fa51e63b172d2f51396cfe141e6ad39222db7965
konesan04 <konesan04@gmail.com>
2025-02-23 14:26:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let input = document.querySelector("input")
+function addvalue(elementval){
+    input.value += elementval
+
+}
+
+function clearval(){
+input.value=""
+}
+
+function delval(){
+    input.value= input.value.slice(0,input.value.length-1)
+}
+
+function equal() {
+    input.value = eval(input.value)
+    
+}
